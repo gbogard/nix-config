7be36c3b1824f6e7e92b1bf5deeb99096f944aa5
Guillaume Bogard <hey@guillaumebogard.dev>
2021-01-10 14:53:50 +0100

    Updated config for multiple computers

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,46 +1,56 @@
-{ config, ... }:
+{ config, lib, ... }:
+let inherit (import ./pkgs.nix) pkgs;
+  machine = (import ./machine.nix);
+in
+lib.mkMerge [
+  {
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
 
-let inherit (import ./pkgs.nix) pkgs; in
-{
-  imports = [
-    ./shell.nix
-    ./neovim/neovim.nix
-    ./programming.nix
-    ./ops.nix
-  ];
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    programs.git = {
+      enable = true;
+      userName = "Guillaume Bogard";
+      userEmail = "hey@guillaumebogard.dev";
+      extraConfig.core.editor = "nvim";
+    };
 
-  programs.git = {
-    enable = true;
-    userName = "Guillaume Bogard";
-    userEmail = "hey@guillaumebogard.dev";
-    extraConfig.core.editor = "nvim";
-  };
+    home.packages = with pkgs; [
+      htop
+      nix-prefetch-scripts
+      nixpkgs-fmt
+      drill
+      curl
+      wget
+      ripgrep
+      youtube-dl
+    ];
 
-  home.packages = with pkgs; [
-    htop
-    nix-prefetch-scripts
-    nixpkgs-fmt
-    drill
-    curl
-    wget
-    ripgrep
-    youtube-dl 
-  ];
-
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "21.03";
+  }
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "guillaume";
-  home.homeDirectory = "/Users/guillaume";
+  (lib.mkIf (machine.hostname == "nananas-xubuntu") {
+    home.username = "guillaune";
+    home.homeDirectory = "/home/guillaume";
+  })
+  (lib.mkIf (machine.hostname == "MBPdeGuillaume") {
+    home.username = "guillaumebogard";
+    home.homeDirectory = "/Users/guillaumebogard";
+  })
+  # Imports
+  (import ./shell.nix { })
+  (import ./neovim/neovim.nix {
+    inherit config;inherit pkgs;inherit lib;
+  })
+  (import ./programming.nix { inherit config;inherit pkgs;inherit lib; })
+  (import ./ops.nix { inherit config;inherit pkgs;inherit lib; })
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.03";
-}
+]
