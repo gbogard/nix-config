2fb1357faea5b7326c69531f3479a253678d7ee1
Guillaume Bogard <hey@guillaumebogard.dev>
2021-03-16 13:14:34 +0100

    Added zsh plugin

--- .config/nixpkgs/bundles/shell.nix
+++ .config/nixpkgs/bundles/shell.nix
@@ -36,6 +36,16 @@ with pkgs; lib.mkMerge [
             sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
           };
         }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.1.0";
+            sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+          };
+        }
       ];
       initExtra =
         # Start starship
@@ -68,6 +78,7 @@ with pkgs; lib.mkMerge [
       exa
       procs
       tokei
+      bash
     ];
   }
   (lib.mkIf (machine.operatingSystem == "Darwin") {
